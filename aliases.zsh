be42419d7b7349f5d0262ddb42f5428275c29a0c
Wojciech Rydel <rydel.wojciech@gmail.com>
2019-04-17 14:42:13 +0200

    Add alias for listening audio from a video link

--- aliases.zsh
+++ aliases.zsh
@@ -6,3 +6,4 @@ export LEDGER_HOME=$HOME/Dropbox/ledger
 alias led="ledger -f $LEDGER_HOME/main.ldg"
 
 alias vldg="vim $LEDGER_HOME/journal.ldg -O $LEDGER_HOME/meta.ldg +"
+alias listen="mpv --no-video"
