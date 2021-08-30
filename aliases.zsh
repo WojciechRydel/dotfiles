1689fff4377595796beea36f36a36f118d1d7c64
Wojciech Rydel <wojciech.rydel@stepstone.com>
2021-08-30 07:47:04 +0200

    Update ledger config

--- aliases.zsh
+++ aliases.zsh
@@ -2,8 +2,8 @@ alias v=vim
 alias rr=ranger
 alias jvim='vim -S ~/.jvim/vimrc'
 
-export LEDGER_HOME=$HOME/Dropbox/ledger
-alias led="ledger -f $LEDGER_HOME/main.ldg"
+export LEDGER_HOME=$HOME/ledger
+alias led="ledger -f $LEDGER_HOME/main.ldg --strict"
 
-alias vldg="vim $LEDGER_HOME/journal.ldg -O $LEDGER_HOME/meta.ldg +"
+alias vldg="vim $LEDGER_HOME/journal.ldg -O $LEDGER_HOME/accounts.ldg +"
 alias listen="mpv --no-video"
