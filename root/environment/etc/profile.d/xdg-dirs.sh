aaad1f8c8894e5c5b8955121e3d0091b9a11acaf
Wojciech Rydel <wojciech@rydel.xyz>
2023-05-26 08:39:10 +0200

    Fix XDG env variables set on login

---
+++ root/environment/etc/profile.d/xdg-dirs.sh
@@ -0,0 +1,7 @@
+# /etc/profile
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
+export XDG_RUNTIME_DIR="/run/user/${UID}"
+
